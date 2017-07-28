2fb73b5fc80a237e326d5dfb5c0a2b7ea17fdb76
GuntyGunty <Gunter0705@yandex.ru>
2017-07-28 16:46:07 +0300

    Тест
    Тест

---
+++ CredinformTest/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.UI.WebControls;
+using System.Web;
+using System.Web.Optimization;
+
+namespace CodeRepository.Web
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			// View/Home/Index.cshtml
+			bundles.Add(new ScriptBundle("~/bundles/home/IndexJS").Include(
+				"~/Scripts/home.js"
+			));
+			bundles.Add(new StyleBundle("~/bundles/home/IndexCSS").Include(
+				"~/Styles/home.css"
+			));
+	
+		}
+	}
+}
---
+++ CredinformTest/Content/blocks/home/home.js
@@ -0,0 +1,5 @@
+﻿import styles from './home.css';
+
+console.log(`Hey.`);
+console.log(`Say my name!`);
+console.log(`You're God damn right.`);
