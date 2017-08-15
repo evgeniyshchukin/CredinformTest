f2914a4ddfa819a3a1f69fafc88e4499d60c2db3
Евгений <e.shchukin@credinform.ru>
2017-08-15 13:13:59 +0300

    eslint

--- CredinformTest/postcss.config.js
+++ CredinformTest/postcss.config.js
@@ -5,28 +5,23 @@ module.exports = {
 		'stylelint': {
 			failAfterError: true
 		},
+		'precss': {},
 		'postcss-import': {},
-		'postcss-simple-vars': {},
 		'css-mqpacker': {},
 		'postcss-normalize': {
 			browsers: ['last 2 versions']
 		},
-		'postcss-cssnext': {
-			browsers: ['last 2 versions', '> 5%']
-		},
 		'postcss-sorting': {
 			"properties-order": sortingConfig
 		},
-		'cssnano': {
-			"preset": [
-				"default",
-				{
-					"discardComments": {
-						"removeAll": true
-					}
-				}
-			]
+		'autoprefixer': {
+			browsers: ['last 2 versions', '> 5%']
 		},
-		'precss': {},
+		'cssnano': {},
+		'postcss-cssnext': {
+			features: {
+				autoprefixer: false
+			}
+		}
 	}
 };
--- CredinformTest/webpack.config.js
+++ CredinformTest/webpack.config.js
@@ -1,7 +1,11 @@
-﻿/// <binding ProjectOpened='Watch - Development, Run - Production, Run - Development' /> 
+﻿/// <binding ProjectOpened='Run - Production' /> 
 
 'use strict';
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+
 const
 		path = require('path'),
 		webpack = require('webpack'),
@@ -46,6 +50,11 @@ const
 				}
 		};
 
+let pathsToClean = [
+	path.resolve(__dirname, 'Images/*.*'),
+	path.resolve(__dirname, 'Scripts/*.*'),
+	path.resolve(__dirname, 'Styles/*.*')
+]
 
 
 switch (environment) {
@@ -67,115 +76,150 @@ switch (environment) {
 }
 
 const main = {
+		devtool: 'source-map',
 		context: path.resolve(__dirname, 'Content/Service'),
 		entry: {
-			home: addresses.home.JS,
-			additional: addresses.additional.JS
+			home: ['whatwg-fetch','babel-polyfill', addresses.home.JS],
+			additional: ['whatwg-fetch', 'babel-polyfill', addresses.additional.JS]
 		},
 		output: {
 				path: path.resolve(__dirname, ''),
 				filename: 'Scripts/[name].js',
-				publicPath: ''
+				publicPath: '/static/'
 		},
+		plugins: [
+			new BrowserSync({
+				host: '192.168.1.177',
+				port: 8082,
+				proxy: 'http://192.168.1.177:8082/'
+			}),
+			new ExtractTextPlugin({
+				filename: (getPath) => {
+					return getPath('Styles/[name].css');
+				},
+				allChunks: true
+			}),
+			new CopyWebpackPlugin([
+				{
+					from: '/Content/img/jpg/*.jpg',
+					to: '/Images/jpg/*.jpg',
+					toType: 'dir'
+				}
+
+			]),
+			new SpritesmithPlugin({
+				src: {
+					cwd: path.resolve(__dirname, 'Content/img/png'),
+					glob: '**/*.png'
+				},
+				target: {
+					image: path.resolve(__dirname, 'Images/png/sprite.png'),
+					css: path.resolve(__dirname, 'Images/png/sprite.css')
+				},
+				apiOptions: {
+					cssImageRef: "~sprite.png"
+				}
+			}),
+			new CleanWebpackPlugin(pathsToClean),
+			new webpack.optimize.OccurrenceOrderPlugin(),
+			new webpack.DefinePlugin({
+				'process.env': {
+					'NODE_ENV': JSON.stringify('production')
+				}
+			}),
+			new webpack.optimize.UglifyJsPlugin({
+				compressor: {
+					warnings: false
+				}
+			}),
+			new HtmlWebpackPlugin({
+				filename: 'Index.html',
+				template: path.resolve(__dirname, 'Views/Home/Index.cshtml'),
+				chunks: ['Home'],
+				inject: false
+			}),
+			new HtmlWebpackPlugin({
+				filename: 'Index.html',
+				template: path.resolve(__dirname, 'Views/Additional/Index.cshtml'),
+				chunks: ['Additional'],
+				inject: false
+			})
+		],
 		module: {
-				rules: [
-						{
-								test: /\.css$/,
-								use: ExtractTextPlugin.extract(
-										{
-												fallback: 'style-loader',
-												use: [{
-														loader: 'css-loader',
-														options: {importLoaders: 1}
-												},
-														'postcss-loader']
-										}
-								)
-						},
-						{
-								test: /\.(gif|png|jpe?g|svg)$/i,
-								loaders: [
-										'file-loader',
-										{
-												loader: 'image-webpack-loader',
-												query: {
-														progressive: true,           
-														optimizationLevel: 3,
-														pngquant: {
-																quality: '65-90',
-																speed: 4
-														}
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /(node_modules|bower_components)/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['env'],
+							plugins: [require('babel-plugin-transform-object-rest-spread')]
+						}
+					}
+				},
+				//{
+				//	test: /\.(htm|html|cshtml)$/i,
+				//	loader: "htmllint-loader",
+				//	include: path.resolve(__dirname, 'Views'),
+				//	query: {
+				//		config: '.htmllintrc',
+				//		failOnError: false,
+				//		failOnWarning: true
+				//	}
+				//},
+				{
+						test: /\.css$/,
+						use: ExtractTextPlugin.extract(
+								{
+										fallback: 'style-loader',
+										use: [{
+												loader: 'css-loader',
+												options: {
+													importLoaders: true
 												}
-
+										},
+												'postcss-loader']
+								}
+						)
+				},
+				{
+						test: /\.(gif|png|jpe?g|svg)$/i,
+						loaders: [
+								'file-loader',
+								{
+										loader: 'image-webpack-loader',
+										query: {
+												progressive: true,           
+												optimizationLevel: 3,
+												pngquant: {
+														quality: '65-90',
+														speed: 4
+												}
 										}
-								]
 
-						},
-						{
-								test: /\.png$/, loaders: [
-										'file-loader?name=i/[hash].[ext]'
-								]
-						},
-						{
-							test: /\.svg$/,
-							loader: 'svg-sprite-loader',
-							include: path.resolve(__dirname, 'Content/img/svg'),
-							options: {
-								extract: true,
-								spriteFilename: 'sprite.svg'
-							}
-						}  
-				]
+								}
+						]
+
+				},
+				{
+						test: /\.png$/, loaders: [
+								'file-loader?name=i/[hash].[ext]'
+						]
+				},
+				{
+					test: /\.svg$/,
+					loader: 'svg-sprite-loader',
+					include: path.resolve(__dirname, 'Content/img/svg'),
+					options: {
+						extract: true,
+						spriteFilename: 'sprite.svg'
+					}
+				}  
+			]
 		},
 		watch: true,
 		watchOptions: {
 				aggregateTimeout: 300
-		},
-
-		plugins: [
-				new BrowserSync({
-						host: '192.168.1.177',
-						port: 8082,
-						proxy: 'http://192.168.1.177:8082/'
-				}),
-				new ExtractTextPlugin({
-						filename: (getPath) => {
-								return getPath('Styles/[name].css');
-						},
-						allChunks: true
-				}),
-				new CopyWebpackPlugin([
-						{
-								from: '/Content/img/jpg/*.jpg',
-								to: '/Images/jpg/*.jpg',
-								toType: 'dir'
-						}
-						
-				]),
-				new SpritesmithPlugin({
-						src: {
-								cwd: path.resolve(__dirname, 'Content/img/png'),
-								glob: '**/*.png'
-						},
-						target: {
-								image: path.resolve(__dirname, 'Images/png/sprite.png'),
-								css: path.resolve(__dirname, 'Images/png/sprite.css')
-						},
-						apiOptions: {
-								cssImageRef: "~sprite.png"
-						}
-				}),
-				new HtmlWebpackPlugin({
-					filename: 'Index.html',
-					template: path.resolve(__dirname, 'Views/Home/Index.cshtml'),
-					chunks: ['Home'],
-					inject: false
-				}),
-				new HtmlWebpackPlugin({
-					filename: 'Index.html',
-					template: path.resolve(__dirname, 'Views/Additional/Index.cshtml'),
-					chunks: ['Additional'],
-					inject: false
-				})
-		]
+		}
 };
